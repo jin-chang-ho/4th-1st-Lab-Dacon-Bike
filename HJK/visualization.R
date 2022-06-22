032e711b76b15538713bbedb12143db1f3c022d6
KrTeaparty <hjk125ha@naver.com>
2022-06-22 16:39:17 +0900

    시각화

--- HJK/visualization.R
+++ HJK/visualization.R
@@ -1,6 +1,14 @@
 library('tidyverse')
+library('lubridate')
+setwd("C:/Users/HJK/Desktop/lab/Dacon_Seoul_Bike/HJK/data")
 source("../functions.R")
 train <- load_df()
 test <- load_test()
 
+plot(train$rental, train$temp_highest)
 
+train.2018 <- train[year(train$date)==2018,]
+
+plot(train.2018$rental, train.2018$temp_highest)
+
+cor(train$rental, train$temp_highest)
